b8b17cb82484266e3f6e3c60050594b43698adc7
GuitarBoy312 <169133828+GuitarBoy312@users.noreply.github.com>
2024-12-12 15:46:58 +0900

    Update 프롬프트 테스트1.py

--- pages/프롬프트 테스트1.py
+++ pages/프롬프트 테스트1.py
@@ -11,7 +11,10 @@ if 'openai_client' not in st.session_state:
 SYSTEM_MESSAGE = {
     "role": "system", 
     "content": '''
-   한국어로 말해줘.
+   You are an elementary school EFL English teacher and your name is Engling. I am an EFL 5th grade elementary school student. 
+   I am an EFL 5th grade elementary school student. 
+   Please practice speaking English with me. My English level is CEFR pre-A1. When you speak English, please speak at a level that suits me. 
+  내가 영어 표현에 대해 물어본 것을 설명할 땐 한국어로 말해줘.
     '''
 }
 
